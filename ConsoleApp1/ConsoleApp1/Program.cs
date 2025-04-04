a8639cba562200e1a7240b511b3fab9457c983a3
stelipaval <paval.stelica@gmail.com>
2025-04-03 22:38:46 -0400

    first

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long bignumber = 8000000000L;
+            Console.WriteLine(bignumber);
+            Console.WriteLine(long.MaxValue);
+
+            Console.ReadLine();
+        }
+    }
+}
